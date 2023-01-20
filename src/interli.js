4af0edb8d67e675e4e62b14eba1cbaebb0fada69
jugosack <jugoslav_ackoski@yahoo.com>
2023-01-20 23:52:30 +0100

    Fixing stylint errors

---
+++ src/interli.js
@@ -0,0 +1,34 @@
+import Todo, { todos } from './todo.js';
+import loadTodo from './todo_loader.js';
+
+const buttonClear = document.querySelector('.clear_btn');
+
+const myFunct = (todo, index) => {
+  todo.id = index;
+};
+
+const updateIndex = () => {
+  todos.forEach(myFunct);
+  Todo.saveTolocalStorage();
+};
+
+const completedTodo = (target) => {
+  const { id } = target.parentElement.parentElement;
+  todos.forEach((todo) => {
+    if (todo.id === +id) todo.completed = !todo.completed;
+  });
+  Todo.saveTolocalStorage();
+  loadTodo();
+};
+
+const clearAllTodo = () => {
+  const newTodos = todos.filter((todo) => todo.completed !== true);
+  Todo.setTolocalStorage(newTodos);
+  Todo.saveTolocalStorage();
+  updateIndex();
+  loadTodo();
+};
+
+buttonClear.addEventListener('click', () => clearAllTodo());
+
+export { completedTodo, clearAllTodo, updateIndex };
